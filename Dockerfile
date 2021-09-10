df12cb041bd0cea33bc4fc30cbd5dcfc606c194c
Adon Metcalfe <adonm@fastmail.fm>
2021-09-10 14:27:07 +0800

    python/ubuntu for ergonomics

--- Dockerfile
+++ Dockerfile
@@ -1,10 +1,10 @@
-FROM alpine
+FROM ubuntu:latest
 LABEL org.opencontainers.image.source https://github.dev/dbca-wa/azcopy-with-acls
 
 WORKDIR /usr/local/bin
-RUN apk --no-cache add curl acl 
-RUN curl -L https://aka.ms/downloadazcopy-v10-linux -o azcopy.tar.gz && tar xvzf azcopy.tar.gz --strip-components=1 && rm azcopy.tar.gz
+RUN apt-get -y update && apt-get -y install curl acl python3 && apt-get clean
+RUN curl -L https://aka.ms/downloadazcopy-v10-linux -o azcopy.tar.gz && \
+    tar xvzf azcopy.tar.gz --strip-components=1 && rm azcopy.tar.gz
+COPY sync.py .
 
-COPY sync.sh sync.sh
-RUN chmod +x sync.sh
-CMD ["/usr/local/bin/sync.sh"]
+CMD ["/usr/local/bin/sync.py"]
---
+++ sync.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+import os
+from subprocess import run
+from sys import exit
+
+local = os.environ.get("LOCAL_PATH", "/mnt/local")
+sas = os.environ["SAS_TOKEN"]
+remote = os.environ["REMOTE_PATH"]
+action = os.environ["ACTION"]
+
+if action == "backup":
+    run(["getfacl", "-R", local, f"{local}.acls"])
+    run(["azcopy", "sync", local, f"{remote}{sas}", "--mirror-mode", "--recursive", "--delete-destination", "--put-md5"])
+    run(["azcopy", "copy", f"{local}.acls", f"{remote}.acls{sas}", "--overwrite"])
+elif action == "restore":
+    if any(os.scandir(local)):
+        exit(f"Files in local path {local}, refusing to restore")
+    run(["azcopy", "copy", f"{remote}.acls{sas}", f"{local}.acls", "--overwrite"])
+    run(["azcopy", "sync", f"{remote}{sas}", local])
+    os.chdir(local)
+    run(["setfacl", f"--restore={local}.acls"])
+else:
+    exit("Please set ACTION (backup or restore), REMOTE_PATH (blob container url) and SAS_TOKEN to sync a volume.")
