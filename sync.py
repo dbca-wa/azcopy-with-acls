b93ac3016092a2df4bff3cf7c1b575e1357a5314
Adon Metcalfe <adonm@fastmail.fm>
2021-09-10 14:29:29 +0800

    sas token properly

--- sync.py
+++ sync.py
@@ -10,13 +10,13 @@ action = os.environ["ACTION"]
 
 if action == "backup":
     run(["getfacl", "-R", local, f"{local}.acls"])
-    run(["azcopy", "sync", local, f"{remote}{sas}", "--mirror-mode", "--recursive", "--delete-destination", "--put-md5"])
-    run(["azcopy", "copy", f"{local}.acls", f"{remote}.acls{sas}", "--overwrite"])
+    run(["azcopy", "sync", local, f"{remote}?{sas}", "--mirror-mode", "--recursive", "--delete-destination", "--put-md5"])
+    run(["azcopy", "copy", f"{local}.acls", f"{remote}.acls?{sas}", "--overwrite"])
 elif action == "restore":
     if any(os.scandir(local)):
         exit(f"Files in local path {local}, refusing to restore")
-    run(["azcopy", "copy", f"{remote}.acls{sas}", f"{local}.acls", "--overwrite"])
-    run(["azcopy", "sync", f"{remote}{sas}", local])
+    run(["azcopy", "copy", f"{remote}.acls?{sas}", f"{local}.acls", "--overwrite"])
+    run(["azcopy", "sync", f"{remote}?{sas}", local])
     os.chdir(local)
     run(["setfacl", f"--restore={local}.acls"])
 else:
