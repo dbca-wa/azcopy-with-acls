f4faa3be99e8805ceba2e4ba7968ec6311afd9a0
Adon Metcalfe <adonm@fastmail.fm>
2021-09-10 14:45:50 +0800

    facls stuff

--- sync.py
+++ sync.py
@@ -3,21 +3,22 @@ import os
 from subprocess import run
 from sys import exit
 
-local = os.environ.get("LOCAL_PATH", "/mnt/local")
+local = os.path.abspath(os.environ.get("LOCAL_PATH", "/mnt/local"))
 sas = os.environ["SAS_TOKEN"]
 remote = os.environ["REMOTE_PATH"]
 action = os.environ["ACTION"]
 
+os.chdir(local)
+
 if action == "backup":
-    run(["getfacl", "-R", local, f"{local}.acls"])
-    run(["azcopy", "sync", local, f"{remote}?{sas}", "--mirror-mode", "--recursive", "--delete-destination", "--put-md5"])
-    run(["azcopy", "copy", f"{local}.acls", f"{remote}.acls?{sas}", "--overwrite"])
+    run(f"getfacl -R . > {local}.acls", shell=True)
+    run(["azcopy", "sync", local, f"{remote}?{sas}", "--mirror-mode", "--recursive", "--delete-destination", "true", "--put-md5"])
+    run(["azcopy", "copy", f"{local}.acls", f"{remote}.acls?{sas}", "--overwrite", "true"])
 elif action == "restore":
     if any(os.scandir(local)):
         exit(f"Files in local path {local}, refusing to restore")
-    run(["azcopy", "copy", f"{remote}.acls?{sas}", f"{local}.acls", "--overwrite"])
+    run(["azcopy", "copy", f"{remote}.acls?{sas}", f"{local}.acls", "--overwrite", "true"])
     run(["azcopy", "sync", f"{remote}?{sas}", local])
-    os.chdir(local)
     run(["setfacl", f"--restore={local}.acls"])
 else:
     exit("Please set ACTION (backup or restore), REMOTE_PATH (blob container url) and SAS_TOKEN to sync a volume.")
