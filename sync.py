f0be66949e8d99a846d3e52eb07667cc94b1c20c
Adon Metcalfe <adonm@fastmail.fm>
2021-11-27 23:17:09 +0800

    azcopy cmds

--- sync.py
+++ sync.py
@@ -11,13 +11,13 @@ action = os.environ["ACTION"]
 os.chdir(local)
 
 if action == "backup":
-    run(f"tar -I lz4 -cvf - . | azcopy cp \"{remote}?{sas}\" --overwrite true", shell=True)
+    run(f"tar -I lz4 -cvf - . | azcopy cp \"{remote}?{sas}\" --from-to=PipeBlob", shell=True)
 elif action.startswith("restore"):
     if os.path.exists("lost+found"): # handle new filesystems
         os.rmdir("lost+found")
     if any(os.scandir(local)) and action != "restore_clobber":
         run(["ls", "-lah"])
         exit(f"Files in local path {local}, refusing to restore")
-    run(f"azcopy cp \"{remote}?{sas}\" | tar -I lz4 -xvf -", shell=True)
+    run(f"azcopy cp \"{remote}?{sas}\"  --from-to=BlobPipe | tar -I lz4 -xvf -", shell=True)
 else:
     exit("Please set ACTION (backup or restore), REMOTE_PATH (blob container url) and SAS_TOKEN to sync a volume.")
